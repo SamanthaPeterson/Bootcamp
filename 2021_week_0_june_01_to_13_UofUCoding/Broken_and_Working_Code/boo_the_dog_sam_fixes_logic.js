07966ce90077f78d4092bc758eb4e6e4ac12d28d
Samantha Peterson <photograher.samantha@live.com>
2021-06-13 18:54:27 -0600

    edits to javascript file boo the dog

--- 2021_week_0_june_01_to_13_UofUCoding/Broken_and_Working_Code/boo_the_dog_sam_fixes_logic.js
+++ 2021_week_0_june_01_to_13_UofUCoding/Broken_and_Working_Code/boo_the_dog_sam_fixes_logic.js
@@ -1,12 +1,12 @@
 $("#factButton").on("click", function() {
-	var number = Math.floor((Math.random() * booFacts.length));
-	$("#factText").text(booFacts[number])
+	var number = Math.floor((Math.random() * booFactsArray.length));
+	$("#factText").text(booFactsArray[number])
 })
 
 var booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books" ]
 
 $("#textPink").on("click", function() {
-	$("#funText").css("color", pink)
+	$("#funText").css("color", "pink")
 })
 
 $("#textOrange").on("click", function() {
@@ -17,11 +17,11 @@ $("#textGreen").on("click", function() {
 })
 
 
-$("#boxGrow").on(click, function() {
+$("#boxGrow").on("click", function() {
 	$("#box").animate({height:"+=35px", 
 		width:"+=35px"}, "fast");
 })
 
-$("#boxShrink").on(click, function() {
+$("#boxShrink").on("click", function() {
 	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
 })
